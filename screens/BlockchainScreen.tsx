dd390a556adb44e670071518e7eb3ff2572e825a
Stanley Chan <0010stanleychan@gmail.com>
2021-01-30 23:03:43 +0800

    Write QR Code

--- screens/BlockchainScreen.tsx
+++ screens/BlockchainScreen.tsx
@@ -1,42 +1,63 @@
-import * as React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import * as React from "react";
+import { ImageBackground, StyleSheet } from "react-native";
+import Timeline from "react-native-timeline-flatlist";
+import { Drizzle } from "@drizzle/store";
+import Simple from "../build/contracts/MyStringStore.json";
+import { View } from "../components/Themed";
 
-import { View } from '../components/Themed';
-import Timeline from 'react-native-timeline-flatlist'
-
-const backgroundImage = '../assets/images/blockchain_page.png'
-const date = '2021/01/31'
+const backgroundImage = "../assets/images/blockchain_page.png";
+const date = "2021/01/31";
 const data = [
-  { time: '30 Jan 09:00', title: 'Lab Status Update', description: 'The Lab has started processing your specimen' },
-  { time: '30 Jan 10:45', title: 'Lab Status Update', description: 'The Lab has started PCR of your specimen' },
-  { time: '30 Jan 12:00', title: `COVID-19 Test Result: ${date}`, description: 'Negative' },
-]
+  {
+    time: "30 Jan 09:00",
+    title: "Lab Status Update",
+    description: "The Lab has started processing your specimen",
+  },
+  {
+    time: "30 Jan 10:45",
+    title: "Lab Status Update",
+    description: "The Lab has started PCR of your specimen",
+  },
+  {
+    time: "30 Jan 12:00",
+    title: `COVID-19 Test Result: ${date}`,
+    description: "Negative",
+  },
+];
 
-const isTestPositive = () => {
-  const testResult = data.find(block => block?.description == "Positive")
-  if (testResult == null) {
-    return false
-  } else {
-    return true
-  }
+const options = {
+  // contracts: [MyStringStore],
+
 }
 
+const isTestPositive = () => {
+  const testResult = data.find((block) => block?.description == "Positive");
+  if (testResult == null) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
 export default function BlockchainScreen() {
-
-
   return (
     <View style={styles.container}>
       <ImageBackground source={require(backgroundImage)} style={styles.image}>
-        <Timeline style={styles.list} circleSize={20}
+        <Timeline
+          style={styles.list}
+          circleSize={20}
           renderFullLine
           circleColor='rgb(45,156,219)'
           lineColor='rgb(45,156,219)'
-          innerCircle={'dot'}
-          descriptionStyle={isTestPositive() ? { color: 'red' } : { color: 'green' }}
+          innerCircle={"dot"}
+          descriptionStyle={
+            isTestPositive() ? { color: "red" } : { color: "green" }
+          }
           options={{
-            style: { paddingTop: 5, paddingLeft: 15 }
+            style: { paddingTop: 5, paddingLeft: 15 },
           }}
-          data={data} />
+          data={data}
+        />
       </ImageBackground>
     </View>
   );
@@ -45,11 +66,11 @@ export default function BlockchainScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30
+    paddingTop: 30,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   list: {
     flex: 1,
@@ -57,7 +78,7 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 });
--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,21 +1,41 @@
-import * as React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
-import { Button, Image } from 'react-native-elements';
+import * as React from "react";
+import { ImageBackground, StyleSheet } from "react-native";
+import { Button, Image } from "react-native-elements";
 
-
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
 
 export default function WelcomeScreen({ navigation }: any) {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('../assets/images/blockchain_page.png')} style={styles.backgroundImage}>
-        <Image source={require('../assets/images/appIcon.png')} style={styles.logo}></Image>
+      <ImageBackground
+        source={require("../assets/images/blockchain_page.png")}
+        style={styles.backgroundImage}
+      >
+        <Image
+          source={require("../assets/images/appIcon.png")}
+          style={styles.logo}
+        ></Image>
         <Text style={styles.title}>Welcome,</Text>
         <Text style={styles.name}>Chan Tai Man</Text>
-        <Button title="Access your Blockchain" onPress={() => navigation.navigate('Blockchain', { screen: 'BlockchainScreen' })} buttonStyle={styles.button} type="outline" />
-        <Button title="Halo Passport" onPress={() => navigation.navigate('Passport', { screen: 'Passport' })} buttonStyle={styles.button} type="outline" disabled />
+        <Button
+          title='Access your Blockchain'
+          onPress={() =>
+            navigation.navigate("Blockchain", { screen: "BlockchainScreen" })
+          }
+          buttonStyle={styles.button}
+          type='outline'
+        />
+        <Button
+          title='Halo Passport'
+          onPress={() =>
+            navigation.navigate("Passport", { screen: "Passport" })
+          }
+          buttonStyle={styles.button}
+          type='outline'
+          disabled
+        />
       </ImageBackground>
-    </View >
+    </View>
   );
 }
 
@@ -25,27 +45,27 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 25,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    marginTop: 30
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginTop: 30,
   },
   name: {
     fontSize: 20,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    marginBottom: 60
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginBottom: 60,
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
   },
   logo: {
     marginBottom: 10,
-    alignSelf: 'center',
-    width: '100%',
+    alignSelf: "center",
+    width: "100%",
     height: 130,
-    resizeMode: 'contain'
+    resizeMode: "contain",
   },
   button: {
     paddingVertical: 10,
@@ -53,5 +73,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     height: 60,
     marginVertical: 15,
-  }
+  },
 });
